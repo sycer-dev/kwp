f3dcd9397670652bb8616c75f2ef6382b9319678
Carter Himmel <carter@elhnet.net>
2020-08-16 19:11:07 -0600

    chore: lint

--- examples/products.rs
+++ examples/products.rs
@@ -2,14 +2,16 @@ use kwp::{Parser, Prefixes};
 
 fn main() {
     let input = "-youth,+hoodie,+hat";
-	let products = vec!["Youth Tee", "Blue Tee - Youth", "Blurple Hoodie", "Wumpus Hat"];
-	
-    let parser = Parser::new(
-        &input,
-        Prefixes::default()
-    );
-	let res = parser.parse();
-	
-	let ext = parser.match_products(products.clone(), res.clone());
+    let products = vec![
+        "Youth Tee",
+        "Blue Tee - Youth",
+        "Blurple Hoodie",
+        "Wumpus Hat",
+    ];
+
+    let parser = Parser::new(&input, Prefixes::default());
+    let res = parser.parse();
+
+    let ext = parser.match_products(products.clone(), res.clone());
     println!("{:#?}\n{:#?}", res, ext);
 }
