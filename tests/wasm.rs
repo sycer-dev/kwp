29448e5d0770938a508d8a6a6ff686ea60c1db47
Carter Himmel <carter@elhnet.net>
2020-08-16 19:07:19 -0600

    feat: wasm

---
+++ tests/wasm.rs
@@ -0,0 +1,74 @@
+use wasm_bindgen_test::*;
+use kwp::{Parser, Prefixes};
+
+#[wasm_bindgen_test]
+fn basic_text() {
+	let parser = Parser::new(
+		"+foo,-bar,+baz",
+		Prefixes {
+			positive: "+",
+			negative: "-",
+		},
+	);
+	let keywords = parser.parse();
+	assert_eq!(keywords.positive, vec!["foo", "baz"]);
+	assert_eq!(keywords.negative, vec!["bar"]);
+}
+
+#[wasm_bindgen_test]
+fn do_not_retain_prefix() {
+	let mut parser = Parser::new(
+		"+foo,-bar,+baz",
+		Prefixes {
+			positive: "+",
+			negative: "-",
+		},
+	);
+	parser.should_retain_prefix(false);
+
+	let keywords = parser.parse();
+	assert_eq!(keywords.positive, vec!["foo", "baz"]);
+	assert_eq!(keywords.negative, vec!["bar"]);
+}
+
+#[wasm_bindgen_test]
+fn weird_prefixes() {
+	let parser = Parser::new(
+		"yes!!foo,no!!bar,yes!!baz",
+		Prefixes {
+			positive: "yes!!",
+			negative: "no!!",
+		},
+	);
+	let keywords = parser.parse();
+	assert_eq!(keywords.positive, vec!["foo", "baz"]);
+	assert_eq!(keywords.negative, vec!["bar"]);
+}
+
+#[wasm_bindgen_test]
+fn unparsed() {
+	let parser = Parser::new(
+		"+foo,-bar,+baz,bak",
+		Prefixes {
+			positive: "+",
+			negative: "-",
+		},
+	);
+	let keywords = parser.parse();
+	assert_eq!(keywords.other, vec!["bak"]);
+}
+
+#[wasm_bindgen_test]
+fn basic_products() {
+	let products = vec!["MyProduct Adult", "MyProduct Youth"];
+	let parser = Parser::new(
+		"+myproduct,-youth",
+		Prefixes {
+			positive: "+",
+			negative: "-",
+		},
+	);
+	let keywords = parser.parse();
+	let products = parser.match_products(products.clone(), keywords.clone());
+	assert_eq!(products, vec!["MyProduct Adult"]);
+}
